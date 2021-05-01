30da11b008c16b0e7cb100e6fb15201a6e376929
lens0021 <lorentz0021@gmail.com>
2021-05-01 14:57:28 +0900

    Comment all test jobs

--- terraform/mediawiki-consul-test.tf
+++ terraform/mediawiki-consul-test.tf
@@ -1,27 +1,27 @@
 # This file is for test. See https://github.com/femiwiki/femiwiki/issues/253 for details.
 
-resource "nomad_job" "mysql_consul_test" {
-  provider = nomad.consul_test
-  # depends_on = [nomad_volume.mysql] # during test period
-  detach  = false
-  jobspec = file("../jobs-consul-test/mysql.nomad")
+# resource "nomad_job" "mysql_consul_test" {
+#   provider = nomad.consul_test
+#   # depends_on = [nomad_volume.mysql] # during test period
+#   detach  = false
+#   jobspec = file("../jobs-consul-test/mysql.nomad")
 
-  hcl2 {
-    enabled  = true
-    allow_fs = true
-  }
-}
+#   hcl2 {
+#     enabled  = true
+#     allow_fs = true
+#   }
+# }
 
-resource "nomad_job" "memcached_consul_test" {
-  provider = nomad.consul_test
-  detach   = false
-  jobspec  = file("../jobs-consul-test/memcached.nomad")
+# resource "nomad_job" "memcached_consul_test" {
+#   provider = nomad.consul_test
+#   detach   = false
+#   jobspec  = file("../jobs-consul-test/memcached.nomad")
 
-  hcl2 {
-    enabled  = true
-    allow_fs = true
-  }
-}
+#   hcl2 {
+#     enabled  = true
+#     allow_fs = true
+#   }
+# }
 
 # During test period
 # resource "nomad_job" "fastcgi_consul_test" {
@@ -40,17 +40,17 @@ resource "nomad_job" "memcached_consul_test" {
 #   }
 # }
 
-resource "nomad_job" "http_consul_test" {
-  provider = nomad.consul_test
-  # depends_on = [nomad_volume.caddycerts] # depends_on = [nomad_volume.mysql] # during test period
-  detach  = false
-  jobspec = file("../jobs-consul-test/http.nomad")
+# resource "nomad_job" "http_consul_test" {
+#   provider = nomad.consul_test
+#   # depends_on = [nomad_volume.caddycerts] # depends_on = [nomad_volume.mysql] # during test period
+#   detach  = false
+#   jobspec = file("../jobs-consul-test/http.nomad")
 
-  hcl2 {
-    enabled  = true
-    allow_fs = true
-  }
-}
+#   hcl2 {
+#     enabled  = true
+#     allow_fs = true
+#   }
+# }
 
 # During test period
 # resource "nomad_job" "parsoid_consul_test" {
@@ -68,24 +68,24 @@ resource "nomad_job" "http_consul_test" {
 #   }
 # }
 
-resource "nomad_job" "restbase_consul_test" {
-  provider = nomad.consul_test
-  detach   = false
-  jobspec  = file("../jobs-consul-test/restbase.nomad")
+# resource "nomad_job" "restbase_consul_test" {
+#   provider = nomad.consul_test
+#   detach   = false
+#   jobspec  = file("../jobs-consul-test/restbase.nomad")
 
-  hcl2 {
-    enabled  = true
-    allow_fs = true
-  }
-}
+#   hcl2 {
+#     enabled  = true
+#     allow_fs = true
+#   }
+# }
 
-resource "nomad_job" "mathoid_consul_test" {
-  provider = nomad.consul_test
-  detach   = false
-  jobspec  = file("../jobs-consul-test/mathoid.nomad")
+# resource "nomad_job" "mathoid_consul_test" {
+#   provider = nomad.consul_test
+#   detach   = false
+#   jobspec  = file("../jobs-consul-test/mathoid.nomad")
 
-  hcl2 {
-    enabled  = true
-    allow_fs = true
-  }
-}
+#   hcl2 {
+#     enabled  = true
+#     allow_fs = true
+#   }
+# }
