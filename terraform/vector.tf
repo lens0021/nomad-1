54b8e8db32363a939425baf0d223d978258b8237
Lens0021 / Leslie <lorentz0021@gmail.com>
2024-03-02 03:01:02 +0900

    Disable Vector.dev (#198)

--- terraform/vector.tf
+++ terraform/vector.tf
@@ -4,6 +4,7 @@ variable "openobserve_password" {
 }
 
 resource "nomad_job" "vector" {
+  count  = 0
   detach = false
   # Note: nomad_variable resource is not available before terraform-provider-nomad v2.0.0
   jobspec = replace(file("../jobs/vector.nomad"), "OPENOBSERVE_PASSWORD", var.openobserve_password)
